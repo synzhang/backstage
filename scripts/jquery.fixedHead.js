664cc82fd68362c7f6f3a865fa538e6f22adeb00
Syn Zhang <zeyanzhang.china@gmail.com>
2013-08-14 15:01:19 +0800

    重构 FixedHead 组件

--- scripts/jquery.fixedHead.js
+++ scripts/jquery.fixedHead.js
@@ -1,107 +1,189 @@
-// IE 6 Fixed Position Hack
+/* ==========================================================================
+ * @author: zeyanzhang.china@gmail.com  (Syn Zhang)
+ * ========================================================================== */
+
+// CSS 中加入以下代码：
+// IE 6 Fixed Position Patch
+//
 // * html, * html body {
 //   background-image: url(about:blank);
 //   background-attachment:fixed;
 // }
 
-// * html .ie6fixedTop {
-//   position: absolute;
-//   /*left: expression(eval(document.documentElement.scrollLeft));*/
-//   top: expression(eval(document.documentElement.scrollTop));
-// }
 (function($) {
-	function createObj(o) {
-		function F() {}
-		F.prototype = o;
-		return new F();
-	}
 
-	FixedHead = {
-		init: function(elem, options) {
-			var self = this;
-			self.$elem = $(elem);
-			self.options = $.extend({}, $.fn.fixedHead.options, options);
+  // FixedHead Public Class Definition
+  // =================================
 
-			self.$head = self.$elem.find(self.options.head);
-			self.$headCloneWraped = $('<' + self.options.wrapper + ' />')
-			.append(self.$head.clone(true))
-			.addClass(self.options.headClass)
-			.hide()
-			.appendTo('body');
-			self.$body = self.$elem.find(self.options.body);
-
-			if ($('html').hasClass('lt-ie7')) {
-				self.$headCloneWraped.addClass('ie6fixedTop').css({
-					'left': function() {
-						return self.$elem.offset().left;
-					}
-				});
-			} else {
-				self.$headCloneWraped.css({
-					'position': 'fixed',
-					'top': 0,
-					'left': function() {
-						return self.$elem.offset().left;
-					}
-				});
-			}
-
-			self.align(self.$head, self.$headCloneWraped.children());
-
-			self.timer = 0;
-			$(window).on('scroll', function() {
-				if (!self.timer) {
-					self.timer = setTimeout(function() {
-						var scrollTop = $(window).scrollTop();
-						self.$headCloneWraped[scrollTop >= self.$head.offset().top && scrollTop <= (self.$body.offset().top + self.$body.height()) ? 'show' : 'hide']();
-						self.timer = 0;
-					}, 100);
-				}
-			}).on('resize', function() {
-				self.align(self.$head, self.$headCloneWraped.children());
-			});
-		},
-
-		align: function($elem, $elemClone) {
-			var self = this;
-
-			for (var i = 0,j = $elemClone.length; i < j; i++) {
-				var $curElem = $elem.eq(i);
-				var $curElemClone = $elemClone.eq(i);
-
-				$curElemClone.css({
-					width: function() {return parseInt($curElem.css('width')) + self.options.gutter;},
-					left: function() {return $curElem.css('left');}
-				});
-
-				if ($curElemClone.children().length !== 0) {
-					self.align($curElem.children(), $curElemClone.children());
-				}
-			}
-		}
-	};
-
-	$.fn.fixedHead = function(options) {
-		var data = this.data('fixedHead');
-		if (data) return data;
-
-		return this.each(function() {
-			var fixedHead = createObj(FixedHead);
-			fixedHead.init(this, options);
-			$(this).data('fixedHead', fixedHead);
-		});
-	};
-
-  // @param head 需要固定显示的元素
-  // @param headClass 可选，添加到固定头部的 css class
-  // @param wrapper 固定头部的包装器元素，为防止固定（Fixed）的设置影响头部元素，将固定（Fixed）设置在包装器上
-  // @param body 需要固定头部提示的主体元素
-  // @param gutter 固定头部内子元素之间的间距  @TODO
-  $.fn.fixedHead.options = {
-    head: '',
-    headClass: '',
-    wrapper: 'div',
-    body: '',
-    gutter: 0
+  var FixedHead = function(element, options) {
+    this.$element = $(element);
+    this.options  = options;
+    this.$target  = $(this.options.target);
+    this.$clone   = null;
   };
+
+  /**
+   * 复制目标元素
+   * 需要改变元素 position 为 fixed。
+   * IE 6 不支持 postion: fixed，IE 7 中 <thead> position: fixed 也有问题。因此
+   * 加多一层包装，修改包装元素的 position。
+   * 如果无需支持 IE 6/7，可替换为注释的代码。
+   * @return void
+   */
+
+  FixedHead.prototype.clone = function() {
+    var self       = this;
+        coordinate = self.$element.offset();
+
+    // IE 6/7 补丁
+    self.$clone = $('<' + self.options.wrapper + ' />')
+                    .addClass(self.options.wrapperClass)
+                    .append(self.$element.clone(true))
+                    .appendTo('body')
+                    .hide();
+    /*self.$clone = self.$element.clone(true)
+                    .insertAfter(self.$element)
+                    .hide();*/
+
+    if (!!window.ActiveXObject && !window.XMLHttpRequest) {
+      // IE 6 补丁，模拟 position: fixed
+      self.$clone.css({
+        'position': 'absolute',
+        'left': coordinate.left
+      });
+
+      $(window).on('scroll.fixedHead.data-api', function() {
+        self.$clone.css('top', function() {
+          return document.documentElement.scrollTop;
+        });
+      });
+    } else {
+      self.$clone.css({
+        'position': 'fixed',
+        'top': 0,
+        'left': coordinate.left
+      });
+    }
+
+    // IE 6/7 补丁
+    self.align(self.$element, self.$clone.children());
+    // self.align(self.$element, self.$clone);
+  };
+
+  /**
+   * 监听
+   * @return void
+   */
+
+  FixedHead.prototype.spy = function() {
+    var self      = this,
+        scrollTop = $(window).scrollTop(),
+        top       = self.$element.offset().top,
+        bottom    = self.$target.offset().top + self.$target.outerHeight() -
+                      self.$element.outerHeight();
+
+    if (!self.$clone) self.clone();
+    self[scrollTop >= top && scrollTop <= bottom ? 'show' : 'hide']();
+  };
+
+  /**
+   * 显示复制的固定元素，并隐藏原始元素
+   * @return void
+   */
+
+  FixedHead.prototype.show = function() {
+    var self = this;
+
+    self.$clone.show();
+    self.$element.css('visibility', 'hidden');
+  };
+
+  /**
+   * 显示原始元素，并隐藏复制的固定元素
+   * @return void
+   */
+
+  FixedHead.prototype.hide = function() {
+    var self = this;
+
+    self.$element.css('visibility', 'visible');
+    self.$clone.hide();
+  };
+
+  /**
+   * 对齐复制的元素
+   * @return void
+   */
+
+  FixedHead.prototype.align = function($element, $clone) {
+    var self = this;
+
+    for (var i = 0, j = $clone.length; i < j; i++) {
+      var $currentElement = $element.eq(i),
+          $currentClone   = $clone.eq(i);
+
+      $currentClone.css('width', $currentElement.css('width'));
+
+      if ($currentClone.children().length !== 0) {
+        self.align($currentElement.children(), $currentClone.children());
+      }
+    }
+  };
+
+  // FixedHead Default Options
+
+  FixedHead.DEFAULTS = {
+    target      : '',    // 固定元素提示的主体元素
+    wrapper     : 'div', // IE 6/7 补丁，固定元素的包装元素
+    wrapperClass: ''     // IE 6/7 补丁，可选，添加到固定元素的 CSS class
+  };
+
+
+  // FixedHead jQuery Plugin Definition
+  // ==================================
+
+  var old = $.fn.fixedHead;
+
+  $.fn.fixedHead = function(option) {
+    return this.each(function() {
+      var $this   = $(this),
+          data    = $this.data('fixedHead'),
+          options = $.extend(
+            {},
+            FixedHead.DEFAULTS,
+            $this.data(),
+            typeof option == 'object' && option
+          );
+
+      if (!data) $this.data('fixedHead', (data = new FixedHead(this, options)));
+      if (typeof option == 'string') data[option]();
+    });
+  };
+
+  $.fn.fixedHead.Constructor = FixedHead;
+
+
+  // FixedHead No Conflict
+  // =====================
+
+  $.fn.fixedHead.noConflict = function() {
+    $.fn.fixedHead = old;
+    return this;
+  };
+
+
+  // FixedHead Data-API
+  // ==================
+
+  var timer = 0;
+
+  $(window).on('scroll.fixedHead.data-api', function() {
+    if (!timer) {
+      timer = setTimeout(function() {
+        $('[data-spy="fixedHead"]').fixedHead('spy');
+        timer = 0;
+      }, 50);
+    }
+  });
+
 })(jQuery);

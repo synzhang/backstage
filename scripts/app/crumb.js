fbbc934f08d852582604591dbb88ebb7efbbc685
Syn Zhang <zeyanzhang.china@gmail.com>
2013-08-25 16:39:21 +0800

    构建 crumb 模块

---
+++ scripts/app/crumb.js
@@ -0,0 +1,48 @@
+define(['jquery'], function($) {
+  var instance;
+
+  function init() {
+    var _separator = ' - ',
+        _crumb = window.top.document.getElementById('crumb');
+
+    /**
+     * 组合导航
+     * @param  { String } title 当前级别导航提示
+     * @param  { Number } level 导航级别
+     * @return { Null }
+     */
+    function _join(title, level) {
+      var crumbs = _crumb.innerHTML.split(_separator),
+          length = crumbs.length;
+
+      crumbs = crumbs.slice(0, level);
+      crumbs.push(title);
+      _crumb.innerHTML = crumbs.join(_separator);
+    }
+
+    return {
+      /**
+       * 元素在激活（点击）时更新导航
+       * @param  { Object }  $element jQuery 对象，触发元素
+       * @param  { Object }  $parent  jQuery 对象，触发元素的父级元素
+       * @param  { Boolean } level    导航级别
+       * @return { Null }
+       */
+      register: function($element, $parent, level) {
+        $element.on('click.crumb', function() {
+          $parent.trigger('click.crumb');
+          _join(this.innerHTML, level);
+        });
+      }
+    }
+  }
+
+  return {
+    getInstance: function() {
+      if (!instance) {
+        instance = init();
+      }
+      return instance;
+    }
+  }
+});
--- scripts/app/main/index.js
+++ scripts/app/main/index.js
@@ -1,86 +1,100 @@
-define(['jquery.accordion', 'jquery.tab'], function() {
-    var $html = $('html'),
-        asideLeftShow = true,
-        offsetOfSidebar = $('#switcher_aside_left').offset().left;
+define(['app/crumb', 'jquery.tab', 'jquery.accordion'], function(breadcrumb) {
+  var $html = $('html'),
+      crumb = breadcrumb.getInstance(),
+      asideLeftShow = true,
+      offsetOfSidebar = $('#switcher_aside_left').offset().left;
 
-    $('.menu-item').on('click', function(e) {
-      $('.menu-item').removeClass('is-menu-item_on');
-      $(this).addClass('is-menu-item_on');
-      $('#crumb').text($(this)
-        .parent('.menu-bd')
-        .siblings('.is-menu-hd_on:visible')
-        .text()
-        + ' - '
-        + $('.is-menu-item_on')
-        .text());
+  // 导航提示 @todo 待优化
+  crumb.register($('.nav_major [data-toggle="tab"]'), $(), 1);
+  crumb.register($('.aside [data-toggle="accordion"]'),
+                  $('.nav_major .is-tab-title_on > a'), 2);
+  $('.aside .is-accordion-head_on').trigger('click.crumb');
+
+  $('#switcher_aside_left').on('click', function() {
+    asideLeftShow = !asideLeftShow;
+    var offset = asideLeftShow ? (0 - offsetOfSidebar) : offsetOfSidebar;
+
+    $('#aside_left')[asideLeftShow ? 'show' : 'hide']();
+
+    $('#switcher_aside_left').css('left', function() {
+      return $(this).offset().left - offset;
     });
 
-    $('#switcher_aside_left').on('click', function() {
-      asideLeftShow = !asideLeftShow;
-      var offset = asideLeftShow ? (0 - offsetOfSidebar) : offsetOfSidebar;
-
-      $('#aside_left')[asideLeftShow ? 'show' : 'hide']();
-
-      $('#switcher_aside_left').css('left', function() {
-        return $(this).offset().left - offset;
-      });
-
-      if (asideLeftShow) {
-        $('#switcher_aside_left').removeClass('is-switcher_vertical_off')
-        .addClass('is-switcher_vertical_on');
-      } else {
-        $('#switcher_aside_left').removeClass('is-switcher_vertical_on')
-        .addClass('is-switcher_vertical_off');
-      }
-
-      $('#maincontainer').css('left', function() {
-        return $(this).offset().left - offset;
-      });
-      if ($html.hasClass('lt-ie7')) {
-        adaptiveWH('.maincontainer', 'width');
-      }
-
-      $('#crumbbar').css('left', function() {
-        return $(this).offset().left - offset;
-      });
-    });
-
-    /**
-     * 兼容性处理
-     */
-    if ($html.hasClass('lt-ie7')) {
-      // IE 6
-      // .aside 高度似乎只有一行高，没自适应剩余高度。
-      adaptiveWH('.aside', 'height');
-      // $(window).on('resize', adaptiveWH('.aside', 'height'));
-      // .maincontainer 宽度超出自身 left 大小的宽度，高度超出自身 top 大小的高度。
-      adaptiveWH('.maincontainer', 'width');
-      adaptiveWH('.maincontainer', 'height');
-      // $(window).on('resize', adaptiveWH('.maincontainer', 'width'));
-      // $(window).on('resize', adaptiveWH('.maincontainer', 'height'));
-      adaptiveWH('.switcher_aside', 'height');
-      // $(window).on('resize', adaptiveWH('.switcher_aside', 'height'));
-    } else if ($html.hasClass('lt-ie8') && !$html.hasClass('lt-ie7')) {
-      // IE 7
-      // .main(iframe) 高度没自适应为 100%。
-      $('.main').css('height', function() {
-          return $(this).parent().height();
-      });
+    if (asideLeftShow) {
+      $('#switcher_aside_left').removeClass('is-switcher_vertical_off')
+      .addClass('is-switcher_vertical_on');
+    } else {
+      $('#switcher_aside_left').removeClass('is-switcher_vertical_on')
+      .addClass('is-switcher_vertical_off');
     }
 
+    $('#maincontainer').css('left', function() {
+      return $(this).offset().left - offset;
+    });
+    if ($html.hasClass('lt-ie7')) {
+      fillWidthHeight('.maincontainer', 'width');
+    }
+
+    $('#crumbbar').css('left', function() {
+      return $(this).offset().left - offset;
+    });
+  });
+
   /**
-   * 自适应剩余宽度或高度
-   * @param  {Object} elem 选择器，指需要自适应宽度或高度的元素。
-   * @param  {String} attr 'width' 或者 'height'。
-   * @return {null}
+   * 兼容性处理
    */
-  function adaptiveWH(elem, attr) {
-    var attr = attr.toLowerCase();
+
+  if ($html.hasClass('lt-ie7')) {
+    // IE 6
+    var params = [
+        {
+          selector: '.aside', // .aside 只有 line-height 的高度，没自适应剩余高度
+          attribute: 'height'
+        },
+        {
+          selector: '.maincontainer', // .maincontainer 宽度超出自身 left 大小的宽度，高度超出自身 top 大小的高度。
+          attribute: 'width'
+        },
+        {
+          selector: '.maincontainer',
+          attribute: 'height'
+        },
+        {
+          selector: '.switcher_aside',
+          attribute: 'height'
+        }
+      ];
+
+    $(window).on('load resize', function() {
+      var length = params.length;
+      for (var i = 0; i < length; i++) {
+        var param = params[i]
+        fillWidthHeight(param.selector, param.attribute);
+      }
+    });
+    $(window).trigger('load');
+  } else if ($html.hasClass('lt-ie8') && !$html.hasClass('lt-ie7')) {
+    // IE 7
+    // .main(iframe) 高度没自适应为 100%。
+    $('.main').css('height', function() {
+      return $(this).parent().height();
+    });
+  }
+
+  /**
+   * 补充剩余宽度或高度
+   * @param  { Object } selector 选择器，指需要自适应宽度或高度的元素。
+   * @param  { String } attribute 'width' 或者 'height'。
+   * @return { Null }
+   */
+
+  function fillWidthHeight(selector, attribute) {
+    var attr = attribute.toLowerCase();
     if (attr === 'width' || attr === 'height') {
-      $(elem).css(attr, function() {
-        return $(this).parent()[attr === 'width' ? 'width' : 'height']()
-        - parseInt($(this).css(attr === 'width' ? 'left' : 'top'), 10)
-        - parseInt($(this).css(attr === 'width' ? 'right' : 'bottom'), 10);
+      $(selector).css(attr, function() {
+        return $(this).parent()[attr == 'width' ? 'width' : 'height']() -
+          parseInt($(this).css(attr == 'width' ? 'left' : 'top'), 10) -
+          parseInt($(this).css(attr == 'width' ? 'right' : 'bottom'), 10);
       });
     }
   }
